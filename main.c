c72625133ce163fa5e9a5013afa4fcb4dfd4b360
liuwenjin <liuwenjin@jspp.com>
2023-11-20 09:25:21 +0800

    feat: update main.c

--- main.c
+++ main.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
-int main(void)
-{
-    printf("%lu\n", sizeof(int));
+// int say(int a[]){
+//     int length;
+//     length=sizeof(a)/sizeof(a[0]); //这样是错误的，得到的结果永远是1
+//     return length;
+// }
+
+int main(void){
+    int arr[] = {1, 2};
+    int* p = arr;
+    for (size_t i = 0; i < 2; i++){
+        printf("%d\n", arr[i]);
+    }
+
+    for (size_t i = 0; i < 2; i++){
+        printf("%d\n", *(arr+i));
+    }
+
+    for (size_t i = 0; i < 2; i++){
+        printf("%d\n", *(p+i));
+    }
+    printf("%lu\n", sizeof(arr)/sizeof(arr[0]));
+    // printf("%lu\n", say(arr));
 }
